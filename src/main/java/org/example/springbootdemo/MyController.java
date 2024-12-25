1269475cd47d8c918d0f92fbcd2e3c6c8c77f2d5
SKYLER.CHOU <skyler.chou@deltaww.com>
2024-12-25 14:54:30 +0800

    feat : 定義 Class，回傳 Json 格式

--- src/main/java/org/example/springbootdemo/MyController.java
+++ src/main/java/org/example/springbootdemo/MyController.java
@@ -4,6 +4,9 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @RestController
 public class MyController {
     @Autowired
@@ -17,7 +20,19 @@ public class MyController {
     }
 
     @RequestMapping("/product")
-    public String product(){
-      return "第一個是蘋果、第二個是橘子";
+    public Store product(){
+        Store store = new Store();
+        List<String> list = new ArrayList<>();
+        list.add("蘋果");
+        list.add("橘子");
+        store.setProductLists(list);
+        return store;
+    }
+
+    @RequestMapping("/user")
+    public Student user(){
+        Student student = new Student();
+        student.setName("Judy");
+        return student;
     }
 }
---
+++ src/main/java/org/example/springbootdemo/Store.java
@@ -0,0 +1,15 @@
+package org.example.springbootdemo;
+
+import java.util.List;
+
+public class Store {
+    public List<String> productLists;
+
+    public List<String> getProductLists() {
+        return productLists;
+    }
+
+    public void setProductLists(List<String> productLists) {
+        this.productLists = productLists;
+    }
+}
